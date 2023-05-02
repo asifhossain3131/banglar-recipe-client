f6027674a401299343d01334bb00a63d1e016842
asifhossain3131 <asifhossain3131@gmail.com>
2023-05-02 17:56:20 +0600

    recipes table done

--- src/pages/private/chefs recipes/ChefRecipes.jsx
+++ src/pages/private/chefs recipes/ChefRecipes.jsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ChefBanner from './ChefBanner';
+import { Table } from 'flowbite-react';
+import RecipeTable from './RecipeTable';
 
 const ChefRecipes = () => {
     const chef=useLoaderData()
     const{chefImage,chefName,likes,recipes,yearOfExperience,shortBio}=chef
-    console.log(chef)
     return (
         <>
            <ChefBanner chef={chef}></ChefBanner> 
+           <div className='mt-12'>
+            <h1 className='font-bold text-4xl text-center'>Chef's Recipes</h1>
+            <Table className='mt-8' striped={true}>
+  <Table.Head>
+    <Table.HeadCell>
+      Recipe name
+    </Table.HeadCell>
+    <Table.HeadCell>
+      Ingradients
+    </Table.HeadCell>
+    <Table.HeadCell>
+      Cooking method
+    </Table.HeadCell>
+    <Table.HeadCell>
+      Rating
+    </Table.HeadCell>
+    <Table.HeadCell>
+      Add To Favorite
+    </Table.HeadCell>
+  </Table.Head>
+
+  <Table.Body className="divide-y">
+    {
+        recipes.map((recipe, i)=><RecipeTable key={i} recipe={recipe}></RecipeTable>)
+        }
+   
+  </Table.Body>
+</Table>
+           </div>
         </>
     );
 };
---
+++ src/pages/private/chefs recipes/RecipeTable.jsx
@@ -0,0 +1,41 @@
+import { Rating, Table } from 'flowbite-react';
+import React, { useState } from 'react';
+
+const RecipeTable = ({recipe}) => {
+    console.log(recipe)
+    const{cookingMethod, ingredients, rating,recipeName}=recipe
+    const [filled,setFilled]=useState(false)
+
+    const handlebookmarked=()=>{
+setFilled(true)
+    }
+    return (
+        <>
+           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+      <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+       {recipeName}
+      </Table.Cell>
+      <Table.Cell>
+        {
+            ingredients.map(ingradient=><li>{ingradient}</li>)
+        }
+      </Table.Cell>
+      <Table.Cell>
+        {cookingMethod}
+      </Table.Cell>
+      <Table.Cell>
+     {rating}
+      </Table.Cell>
+      <Table.Cell>
+       <button disabled={filled} onClick={handlebookmarked}>
+<Rating size='lg'>
+<Rating.Star filled={filled} />
+</Rating>
+       </button>
+      </Table.Cell>
+    </Table.Row> 
+        </>
+    );
+};
+
+export default RecipeTable;
